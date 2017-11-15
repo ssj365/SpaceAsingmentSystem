f8ce7799adeb640d71bbe6014d40f25a5e3f7a15
joseph <Josephagibs@gmail.com>
2017-11-15 14:09:21 -0330

    A test commit

--- src/SpaceAssignmentSystem/SpaceAssignmentSystem.java
+++ src/SpaceAssignmentSystem/SpaceAssignmentSystem.java
@@ -1,7 +1,7 @@
 package SpaceAssignmentSystem;
 
 import SpaceAssignmentSystem.guiBuilder;
-
+// Some comments here, to test git hub
 public class SpaceAssignmentSystem {
 	public static void main(String[] args) {
 		// scheduler the rendering event.
