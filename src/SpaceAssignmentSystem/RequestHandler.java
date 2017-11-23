ffa18db5612b5041054b78f894f098251a4ebe91
alex <alex@kippermachine.localdomain>
2017-11-23 17:13:53 -0330

    test

--- src/SpaceAssignmentSystem/RequestHandler.java
+++ src/SpaceAssignmentSystem/RequestHandler.java
@@ -1,5 +1,114 @@
 package SpaceAssignmentSystem;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class RequestHandler {
-	// To Do later
+	private Scheduler schedule;
+	private ArrayList<Request> pending;
+	private Queue<Request> denied;
+	
+	public RequestHandler(Scheduler s) {
+		schedule = s;
+		pending = new ArrayList<Request>();
+		denied = new ArrayBlockingQueue<Request>(30);
+	}
+	
+	public Map<String, Booking[]> getFullDay(Date d) {
+		Map<String, Booking[]> m = new HashMap<String, Booking[]>();
+		for(String s : schedule.roomNames()) {
+			ArrayList<Booking> bookings = new ArrayList<Booking>();
+			for(Booking b : schedule.getRoom(s).bookings) {
+				if(b.end.month == d.month && b.end.day == d.day) {
+					bookings.add(b);
+				}
+			}
+			m.put(s, (Booking[]) bookings.toArray());
+		}
+		return m;
+	}
+	
+	public Map<String, Booking[]> getNoConflict(Date d) {
+		Map<String, Booking[]> m = getFullDay(d);
+		ArrayList<Booking> noConflict = new ArrayList<Booking>();
+		for(String s : m.keySet() ) {
+			Booking[] conflict = m.get(s);
+			noConflict.clear();
+			for(Booking b1 : conflict) {
+				for(Booking b2 : noConflict) {
+					if( b1.start.after(b2.start) && b1.start.before(b2.end) ) {
+						noConflict.remove(b2);
+					}
+					if( b1.end.after(b2.start) && b1.end.before(b2.end) ) {
+						noConflict.remove(b2);
+					}
+					else {
+						noConflict.add(b1);
+					}
+				}
+			}
+			m.put(s, (Booking[]) noConflict.toArray());
+		}
+		return m;
+	}
+	
+	public Map<String, Booking[]> getConflict(Date d){
+		Map<String, Booking[]> m = getFullDay(d);
+		ArrayList<Booking> noConflict = new ArrayList<Booking>();
+		for(String s : m.keySet() ) {
+			ArrayList<Booking> conflict = new ArrayList<Booking>();
+			for(Booking b : m.get(s)) {
+				conflict.add(b);
+			}
+			noConflict.clear();
+			for(Booking b1 : conflict) {
+				for(Booking b2 : noConflict) {
+					if( b1.start.after(b2.start) && b1.start.before(b2.end) ) {
+						noConflict.remove(b2);
+					}
+					if( b1.end.after(b2.start) && b1.end.before(b2.end) ) {
+						noConflict.remove(b2);
+					}
+					else {
+						noConflict.add(b1);
+					}
+				}
+			}
+			conflict.removeAll(noConflict);
+			m.put(s, (Booking[]) conflict.toArray());
+		}
+		return m;	
+	}
+	
+	public void newRequest(Request r) {
+		pending.add(r);
+	}
+	
+	public void denyRequest(Request r) {
+		pending.remove(r);
+		denied.add(r);
+	}
+	
+	public boolean approveRequest(Request r) throws SchedulerException {
+		Room room = schedule.getRoom(r.room);
+		for(Booking b : room.bookings) {
+			if( b.start.after(r.booking.start) && b.start.before(r.booking.end) ) {
+				throw new SchedulerException();
+			}
+			if( b.end.after(r.booking.start) && b.end.before(r.booking.end) ) {
+				throw new SchedulerException();
+			}
+			else{
+				return room.bookings.add(r.booking);
+			}
+		}
+		return false;				
+	}
+	
+	public void rejectRequest() {}
+	
+	
 }
--- src/SpaceAssignmentSystem/Scheduler.java
+++ src/SpaceAssignmentSystem/Scheduler.java
@@ -4,24 +4,37 @@ import java.io.*;
 
 public class Scheduler {
 	private Room[] rooms;
-	public String[] names = {"room1", "room2", "room3", "room4", "room5"};
+	private String[] names = {"room1", "room2", "room3", "room4", "room5"};
 	
 	public Scheduler(){ 
-		rooms = new Room[5];
+		rooms = new Room[names.length];
 		for (String s : names) {
-			System.out.println(s);
 			build(s);
 		}
 	}
 	
-	public boolean addBooking(Request r) {
-		int i = pick(r.room);
-		return rooms[i].addBooking(r.booking);
+	public Scheduler(String[] nms){ 
+		names = nms;
+		rooms = new Room[names.length];
+		for (String s : names) {
+			build(s);
+		}
+	}
+	
+	public RequestHandler createRequestHandler() {
+		return new RequestHandler(this);
+	}
+	
+	public Room getRoom(String s) { 
+		return rooms[pick(s)];
+	}
+	
+	public String[] roomNames() {
+		return names;
 	}
 	
 	private void build(String s) {
 		int i = pick(s);
-		System.out.println(i);
 		try {
 			FileInputStream fileIn = new FileInputStream(s + ".ser");
 			ObjectInputStream in = new ObjectInputStream(fileIn);
@@ -30,33 +43,20 @@ public class Scheduler {
 			in.close();
 		}
 		catch(Exception e) {
-			rooms[i] = new Room();
+			rooms[i] = new Room(s);
 		}
 	}
 	
-	private boolean blackOut(Request r) {
-		return false;
-		//to do
-	}
-	
-	private int pick(String s) {
-		if( s.equals("room1") ) {
-			return 0;
+	private int pick(String s1) {
+		int i = 0;
+		for(String s2 : names) {
+			if( s1.equals(s2) ) {
+				return i;
+			}
+			else {
+				i++;
+			}
 		}
-		else if( s.equals("room2") ) {
-			return 1;
-		}
-		else if( s.equals("room3") ) {
-			return 2;
-		}
-		else if( s.equals("room4") ) {
-			return 3;
-		}
-		else if( s.equals("room5") ) {
-			return 4;
-		}
-		else{
-			return -1;			
-		}
+		return -1;
 	}
 }
